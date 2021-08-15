27551718a7f5b9f3714fabb83296b919557e91b8
seunghyecho <tmdgp523@naver.com>
2021-08-15 17:28:59 +0900

    qa - cursor custom js 적용

--- js/cursor.js
+++ js/cursor.js
@@ -1,9 +1,21 @@
- 
- var cursor = document.querySelector("#cursor");
 
- document.addEventListener("mouseout", function (e) {
-   cursor.setAttribute(
-     "style",
-     "top: " + (e.clientY - 10) + "px; left: " + (e.clientX - 10) + "px;"
-   );
- });
+jQuery(document).ready(function () {
+
+  $(document)
+    .mousemove(function (e) {
+      $('.cursor')
+        .eq(0)
+        .css({
+          left: e.clientX,
+          top: e.clientY
+        });
+      setTimeout(function () {
+        $('.cursor')
+          .eq(1)
+          .css({
+            left: e.clientX,
+            top: e.clientY
+          });
+      }, 100);
+    })
+});

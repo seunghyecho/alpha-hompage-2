e7c9b810600685b625a09542e0a09a2c3a9a1215
csh <csh@fles.co.kr>
2021-08-23 14:31:49 +0900

    vertical js 수정

--- js/vertical.js
+++ js/vertical.js
@@ -1,15 +1,21 @@
+// pc progressbar 높이 540
 $(document).ready(function () {
   var $slider_pc = $(".section04 .pc .scroll-slider");
   var $progressBar = $(".section04 .pc .progress");
   var $progressBarLabel = $(".section04 .pc .slider__label");
 
-  $slider_pc.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
-    var calc = 25 + (nextSlide / slick.slideCount) * 100;
+  $slider_pc.on(
+    "beforeChange",
+    function (event, slick, currentSlide, nextSlide) {
+      if (event.cancelable) event.preventDefault();
 
-    $progressBar.css("height", calc * 540 * 0.01);
+      var calc = 25 + (nextSlide / slick.slideCount) * 100;
 
-    $progressBarLabel.text(calc + "% completed");
-  });
+      $progressBar.css("height", calc * 540 * 0.01);
+      console.log($progressBar);
+      $progressBarLabel.text(calc + "% completed");
+    }
+  );
 
   $slider_pc.slick({
     slidesToShow: 1,
@@ -21,17 +27,25 @@ $(document).ready(function () {
     arrows: false,
     cssEase: "linear",
   });
+});
+
+// mb progressbar 높이 200
+$(document).ready(function () {
   var $slider_mb = $(".section04 .mb .scroll-slider");
   var $progressBar = $(".section04 .mb .progress");
   var $progressBarLabel = $(".section04 .mb .slider__label");
 
-  $slider_mb.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
-    var calc = 25 + (nextSlide / slick.slideCount) * 100;
+  $slider_mb.on(
+    "beforeChange",
+    function (event, slick, currentSlide, nextSlide) {
+      if (event.cancelable) event.preventDefault();
+      var calc = 25 + (nextSlide / slick.slideCount) * 100;
 
-    $progressBar.css("height", calc * 200 * 0.01);
+      $progressBar.css("height", calc * 200 * 0.01);
 
-    $progressBarLabel.text(calc + "% completed");
-  });
+      $progressBarLabel.text(calc + "% completed");
+    }
+  );
 
   $slider_mb.slick({
     slidesToShow: 1,
